4b6f4c4cbfe5a2bf79b0983023e4dbd88775b7f7
Adi Maaita <adi.maaita80@gmail.com>
2021-04-26 17:36:31 +0300

    Completed GetDepartmentsFromDB

--- WebServiceTutorialClient/Form1.Designer.cs
+++ WebServiceTutorialClient/Form1.Designer.cs
@@ -29,13 +29,122 @@ namespace WebServiceTutorialClient
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.txtTestWebService = new System.Windows.Forms.TextBox();
+            this.txtJsonData = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtTestWebService
+            // 
+            this.txtTestWebService.Enabled = false;
+            this.txtTestWebService.Location = new System.Drawing.Point(319, 53);
+            this.txtTestWebService.Name = "txtTestWebService";
+            this.txtTestWebService.Size = new System.Drawing.Size(541, 30);
+            this.txtTestWebService.TabIndex = 0;
+            // 
+            // txtJsonData
+            // 
+            this.txtJsonData.Location = new System.Drawing.Point(319, 107);
+            this.txtJsonData.Multiline = true;
+            this.txtJsonData.Name = "txtJsonData";
+            this.txtJsonData.Size = new System.Drawing.Size(541, 191);
+            this.txtJsonData.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(129, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(184, 23);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Hello World Message";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(115, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(198, 23);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "GetDepartments JSON";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(114, 323);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(199, 23);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "GetDepartments Table";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(319, 323);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(541, 194);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(47, 542);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(266, 23);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "GetDepartmentsFromDB Table";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(319, 542);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(541, 172);
+            this.dataGridView2.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(872, 757);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtJsonData);
+            this.Controls.Add(this.txtTestWebService);
+            this.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "HR Service Client Application";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtTestWebService;
+        private System.Windows.Forms.TextBox txtJsonData;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dataGridView2;
     }
 }
 
--- WebServiceTutorialClient/Form1.cs
+++ WebServiceTutorialClient/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json;
 
 namespace WebServiceTutorialClient
 {
@@ -15,6 +16,20 @@ namespace WebServiceTutorialClient
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            HRApp.WebServiceHR proxy = new HRApp.WebServiceHR();
+
+
+            txtTestWebService.Text = proxy.HelloWorld();
+
+            txtJsonData.Text = proxy.GetDepartments();
+
+            dataGridView1.DataSource = JsonConvert.DeserializeObject<DataTable>(proxy.GetDepartments());
+
+            dataGridView2.DataSource = JsonConvert.DeserializeObject<DataTable>(proxy.GetDepartmentsFromDB());
         }
     }
 }
